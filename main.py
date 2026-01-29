a06ec5e499c32b0d22ca8f7fc2de2a62969b4a88
AshtonFabby <ashtonfabbt@gmail.com>
2026-01-29 13:18:42 +0200

    added cors

--- main.py
+++ main.py
@@ -31,7 +31,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://bank-statement-parser-frontend.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
